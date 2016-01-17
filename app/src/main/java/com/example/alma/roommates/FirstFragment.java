d7c9daf8dd7cece1f9e6eaaf0638d8489af84574
Alma Shalom <almash929@hotmail.com>
2016-01-17 20:02:22 +0200

    style changes of colors and theme change navigation header add tabbed fragments deleting unnecessary classes and layouts

--- app/src/main/java/com/example/alma/roommates/FirstFragment.java
+++ app/src/main/java/com/example/alma/roommates/FirstFragment.java
@@ -1,18 +1,17 @@
 package com.example.alma.roommates;
 
-/**
- * Created by alma on 10/01/2016.
- */
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/**
+ * Created by alma on 17/01/2016.
+ */
+public class FirstFragment extends android.support.v4.app.Fragment {
 
-public class OneFragment extends Fragment{
 
-    public OneFragment() {
+    public FirstFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +24,6 @@ public class OneFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        return inflater.inflate(R.layout.first_fragment, container, false);
     }
-
 }
--- app/src/main/java/com/example/alma/roommates/Welcome.java
+++ app/src/main/java/com/example/alma/roommates/Welcome.java
@@ -1,138 +1,90 @@
 package com.example.alma.roommates;
 
-/**
- * Created by alma on 15/12/2015.
- */
-import com.facebook.AccessToken;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
-
-import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * Created by alma on 15/12/2015.
+ */
 
 import de.hdodenhof.circleimageview.CircleImageView;
-
 
 public class Welcome extends AppCompatActivity {
 
     // Declare Variable
-    TextView userName,userApartmentId,userEmail;
-    CircleImageView profileImage;
-    DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    String[]titles = {"RoomMates","My Expenses", "My Tasks", "My Balance", "Dashboard"};
-    private CharSequence mTitle;
-    private CharSequence mDrawerTitle;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private Toolbar topToolBar;
+    private TextView userName,userApartmentId,userEmail;
+    private CircleImageView profileImage;
     // Retrieve current user from Parse.com
     private ParseUser currentUser = ParseUser.getCurrentUser();
-    //FragmentManager mFragmentManager;
-    //FragmentTransaction mFragmentTransaction;
+    // view variables
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
-        mTitle = mDrawerTitle = getTitle();
 
-        topToolBar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(topToolBar);
-        topToolBar.setLogo(R.drawable.home);
-        topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = setupDrawerToggle();
+
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.setDrawerListener(mDrawerToggle);
+
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
 
         /**
-         *Setup the DrawerLayout and NavigationView
+         * Lets inflate the very first fragment
+         * Here , we are inflating the ExpensesTabFragment as the first Fragment
          */
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.flContent,new ExpensesTabFragment()).commit();
 
-
-        //mNavigationView = (NavigationView) findViewById(R.id.shitstuff) ;
-        LayoutInflater inflater = getLayoutInflater();
-        View listHeaderView = inflater.inflate(R.layout.header_list,null, false);
-        mDrawerList.addHeaderView(listHeaderView);
-
-        List<ItemObject> listViewItems = new ArrayList<ItemObject>();
-        listViewItems.add(new ItemObject("My Expenses", R.drawable.dollar));
-        listViewItems.add(new ItemObject("My Tasks", R.drawable.tasks));
-        listViewItems.add(new ItemObject("My Balance", R.drawable.balance));
-        listViewItems.add(new ItemObject("Dashboard", R.drawable.chat));
-
-        mDrawerList.setAdapter(new CustomAdapter(this, listViewItems));
-
-        mDrawerToggle = new ActionBarDrawerToggle(Welcome.this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
-
-            /** Called when a drawer has settled in a completely closed state.*/
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            /** Called when a drawer has settled in a completely open state.*/
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mDrawerToggle.setDrawerIndicatorEnabled(true);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // make Toast when click
-                Toast.makeText(getApplicationContext(), "Position " + position, Toast.LENGTH_LONG).show();
-                selectItemFragment(position);
-            }
-        });
         loadUserDetailsFromParse();
     }
 
     private void loadUserDetailsFromParse(){
         //define buttons and views
-        userName = (TextView)findViewById(R.id.profile_name);
-        userApartmentId = (TextView)findViewById(R.id.profile_apartment_id);
-        userEmail = (TextView)findViewById(R.id.profile_email);
-        profileImage = (CircleImageView)findViewById(R.id.profile_image);
+        View header = nvDrawer.getHeaderView(0);
+        userName = (TextView) header.findViewById(R.id.profile_name);
+        userEmail = (TextView) header.findViewById(R.id.profile_email);
+        userApartmentId = (TextView) header.findViewById(R.id.profile_apartment_id);
+        profileImage = (CircleImageView) header.findViewById(R.id.profile_image);
+
         // Get current user details
         String myUserName = currentUser.getUsername().toString();
         String myUserMail = currentUser.getEmail().toString();
         String myUserImageUrl = currentUser.getString("image");
         String myUserAppId = Apartment.getApartmentId(currentUser);
+
         //Set user details to view
         userName.setText(myUserName);
         userEmail.setText(myUserMail);
@@ -140,67 +92,63 @@ public class Welcome extends AppCompatActivity {
         Picasso.with(Welcome.this).load(myUserImageUrl).resize(200, 200).centerCrop().into(profileImage);
     }
 
-    private void selectItemFragment(int position){
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the planet to show based on
+        // position
         Fragment fragment = null;
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        switch(position) {
+
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = ExpensesTabFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = FirstFragment.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = FirstFragment.class;
+                break;
             default:
-            case 0:
-                fragment = new OneFragment();
-                break;
-            case 1:
-                fragment = new TwoFragment();
-                break;
-            case 2:
-                fragment = new DefaultFragment();
-                break;
-            case 3:
-                fragment = new DefaultFragment();
-                break;
-            case 4:
-                fragment = new DefaultFragment();
-                break;
+                fragmentClass = FirstFragment.class;
         }
-        fragmentManager.beginTransaction().replace(R.id.main_fragment_container, fragment).commit();
-        mDrawerList.setItemChecked(position, true);
-        setTitle(titles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-        /*
-        FragmentTransaction xfragmentTransaction = fragmentManager.beginTransaction();
-        xfragmentTransaction.replace(R.id.main_fragment_container, new TwoFragment()).commit();
-        */
-    }
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getSupportActionBar().setTitle(mTitle);
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
     }
 
+    // Make sure this is the method with just `Bundle` as the signature
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
         mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -227,10 +175,20 @@ public class Welcome extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void setScreenView(){
-        // Get the view
-        setContentView(R.layout.welcome);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
+
+    /*
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        return super.onPrepareOptionsMenu(menu);
+    }*/
+
 }
